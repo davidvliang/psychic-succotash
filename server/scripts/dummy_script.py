ca68ffaa301099bafcafbfd6ae406a0c2527d5cd
david947 <david947@hawaii.edu>
2023-07-17 09:36:33 -1000

    clean up

--- server/scripts/dummy_script.py
+++ server/scripts/dummy_script.py
@@ -3,42 +3,8 @@ import json
 
 def pretty_print_array(arr, dim):
     int_arr = [1 if x else 0 for x in arr]
-    arr_string = ""
-    for i, en in enumerate(int_arr):
-        if (i+1)%dim:
-            arr_string = arr_string+f"{en} "
-        else:
-            arr_string = arr_string+f"{en}\n\t\t     "
-    return arr_string   
-    # def process_json_as_file(json_path):
-    #     """_summary_
-
-    #     Args:
-    #         json_path (string): path to JSON file
-
-    #     Returns:
-    #         timestamp: Time the command was called on the web interface Used as identifier.
-    #         pos_voltage: User-specified positive voltage (V). Between -10 and 10.
-    #         neg_voltage: Negative voltage (V). Between -10 and 10.
-    #         frequency: Frequency (Hz)
-    #         duty_cycle: Duty cycle (whole %). Between 0 and 100.
-    #         dmux_output_num: Python dict to configure cells 0 to 15  
-
-    #     """
-
-    #     with open(json_path, "r") as json_file:
-    #         data = json.load(json_file)
-    #         index = -1  # grab the most recent entry
-
-    #         timestamp = data[index]['timestamp']
-    #         pos_voltage = float(data[index]['posVoltage'])
-    #         neg_voltage = float(data[index]['negVoltage'])
-    #         frequency = int(data[index]['frequency'])
-    #         duty_cycle = int(data[index]['dutyCycle'])
-    #         default_duration = int(data[index]['defaultDuration'])
-    #         dmux_output_num = dict(data[index]['dmuxOutputNum'])
-
-    #     return timestamp, pos_voltage, neg_voltage, frequency, duty_cycle, default_duration, dmux_output_num
+    arr_string = [f"{en} " if ((i+1)%dim) else f"{en}\n\t\t     " for i, en in enumerate(int_arr)]
+    return ''.join(arr_string)   
 
 def process_input_as_json(json_input):
     """this does that thing.
