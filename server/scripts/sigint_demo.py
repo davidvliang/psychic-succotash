3c5ff3ca5d020431686810218a3aa97c4e41d63a
David Liang <34115092+davidvliang@users.noreply.github.com>
2023-08-01 18:36:00 -1000

    add signal handler example

--- server/scripts/sigint_demo.py
+++ server/scripts/sigint_demo.py
@@ -3,44 +3,46 @@ import os
 import time
 import sys
 
-# def handler(signum, frame):
-#     signame = signal.Signals(signum).name
-#     print(f"Signal Handler called with signal {signame} ({signum})")
-#     exit()
-#     # raise OSError("Couldn't open device or sumshit")
-
-# signal.signal(signal.SIGINT, handler)
-# # signal.alarm(5)
-# a = True
-# # time.sleep(10)
-# while(a):
-#     print("a",end=" ")
-#     # time.sleep(1)
-# a = False
-# exit()
-
-def off():
-    print("hello there")
-
-
-try: 
-    while True:
-        # print("b" , end=" ", flush=True)
-        sys.stdout.write("b ")
-        sys.stdout.flush()
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("you pressed CTRL+C!!!", flush=True)
-    sys.stdout.write("b ")
-    sys.stdout.flush()
+def handler(signum, frame):
+    signame = signal.Signals(signum).name
     time.sleep(1)
-finally:
-    print("do all this finally stuff1", flush=True)
-    print("do all this finally stuff2", flush=True)
-    print("do all this finally stuff3", flush=True)
-    print("do all this finally stuff4", flush=True)
-    off()
-    sys.stdout.write("do all this finally stuff5\n")
-    sys.exit("hello")
+    print(f"Signal Handler called with signal {signame} ({signum})")
+    time.sleep(1)
+    exit()
+    # raise OSError("Couldn't open device or sumshit")
 
-print("Done.", flush=True)
+signal.signal(signal.SIGTERM, handler)
+# signal.alarm(5)
+a = True
+# time.sleep(10)
+while(a):
+    print("a",end=" ", flush=True)
+    time.sleep(1)
+a = False
+exit()
+
+# def off():
+#     print("hello there")
+
+
+# try: 
+#     while True:
+#         # print("b" , end=" ", flush=True)
+#         sys.stdout.write("b ")
+#         sys.stdout.flush()
+#         time.sleep(1)
+# except KeyboardInterrupt:
+#     print("you pressed CTRL+C!!!", flush=True)
+#     sys.stdout.write("b ")
+#     sys.stdout.flush()
+#     time.sleep(1)
+# finally:
+#     print("do all this finally stuff1", flush=True)
+#     print("do all this finally stuff2", flush=True)
+#     print("do all this finally stuff3", flush=True)
+#     print("do all this finally stuff4", flush=True)
+#     off()
+#     sys.stdout.write("do all this finally stuff5\n")
+#     sys.exit("hello")
+
+# print("Done.", flush=True)
