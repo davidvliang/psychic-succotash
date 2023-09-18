8720630e3e1f04018a1c12dfaef228f08190d0fa
David Liang <34115092+davidvliang@users.noreply.github.com>
2023-09-17 21:49:23 -1000

    simplify DAQ inputs

--- client/src/utils/DAQ.tsx
+++ client/src/utils/DAQ.tsx
@@ -6,24 +6,7 @@ export type DAQInputs = {
   dutyCycle: number;
   defaultDuration: number;
   arrSize: number;
-  dmuxOutputNum: {
-    "0": number;
-    "1": number;
-    "2": number;
-    "3": number;
-    "4": number;
-    "5": number;
-    "6": number;
-    "7": number;
-    "8": number;
-    "9": number;
-    "10": number;
-    "11": number;
-    "12": number;
-    "13": number;
-    "14": number;
-    "15": number;
-  }; // from cell array form
+  dmuxOutputNum: string[];
 };
 
 export const defaultActuatedCells = {
